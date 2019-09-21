2815250a508ecdd9c2092e3b9bc3ca587048f69a
Eddie9719 <eddieverdesoto@hotmail.com>
2019-09-21 16:04:17 -0500

    Update

--- Proyecto/ProyectoNT/resources/views/categorias/index.blade.php
+++ Proyecto/ProyectoNT/resources/views/categorias/index.blade.php
@@ -60,7 +60,6 @@
 
 
         </div>
-     <!-- Agregar Categoría-->   
         <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog model-notify modal-lg modal-right modal-success" role="document">
     <div class="modal-content">
@@ -81,31 +80,6 @@
         <button type="button" class="btn btn-warning" data-dismiss="modal">Cerrar</button>
         <button type="submit" class="btn btn-success" value="Agregar" >Agregar</button>
       </div>
-        <!-- Editar Categoría-->  
-      <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog model-notify modal-lg modal-right modal-success" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Ingreso de Datos</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-      <form action="{{url('/categorias')}}" method="post">
-        {{ csrf_field() }}
-        <label for="Nombre">{{'Nombre de la Categoría'}}</label>
-    <input type="text" name="Nombre" id="Nombre" value="">
-        <br/> 
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-warning" data-dismiss="modal">Cerrar</button>
-        <button type="submit" class="btn btn-success" value="Agregar" >Agregar</button>
-      </div>
-
-        <!-- Eliminar Categoría-->  
-
-
       </form>
     </div>
   </div>
