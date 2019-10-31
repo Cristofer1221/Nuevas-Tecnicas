1011565b6daec040d19d6cebe11860475d8aed95
Alejandro Erazo <adrian.alejo1994@hotmail.com>
2019-10-31 15:01:29 -0500

    correccion texto

--- BenchMark/resources/views/home.blade.php
+++ BenchMark/resources/views/home.blade.php
@@ -156,13 +156,7 @@
             <p class="text-muted"></p>
           </div>
         </div>
-        <div class="col-md-4 col-sm-6 portfolio-item">
-          <a class="portfolio-link" data-toggle="modal" href="#portfolioModal6">
-            <div class="portfolio-hover">
-              <div class="portfolio-hover-content">
-                <i class="fas fa-plus fa-3x"></i>
-              </div>
-            </div>
+
 
 
   </section>
@@ -180,23 +174,15 @@
       </div>
       <div class="row text-center">
         <div class="col-md-4">
-          <span class="fa-stack fa-4x">
+        <span class="fa-stack fa-4x">
             <i class="fas fa-circle fa-stack-2x text-primary"></i>
-            <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
+            <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
           </span>
           <h4 class="service-heading">Consulta donde Adquirir Hardware</h4>
           <p class="text-muted"> <a href="https://www.mtec-ec.com/"> Click Aquí</a> </p>
         </div>
-        <div class="col-md-4">
-          <!--<span class="fa-stack fa-4x">
-            <i class="fas fa-circle fa-stack-2x text-primary"></i>
-            <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
-            <i class="fas fa-circle fa-stack-2x text-primary"></i>
-            <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
-          </span> -->
-          <h4 class="service-heading"></h4>
-          <p class="text-muted"></p>
-        </div> 
+
+
         <div class="col-md-4">
           <span class="fa-stack fa-4x">
             <i class="fas fa-circle fa-stack-2x text-primary"></i>
@@ -205,6 +191,10 @@
           <h4 class="service-heading">Consulta Información sobre Hardware Específico</h4>
           <p class="text-muted"> <a href="/cpu"> Click Aquí</a> </p></div>
       </div>
+      </div>
+
+
+      
     </div>
   </section>
 
