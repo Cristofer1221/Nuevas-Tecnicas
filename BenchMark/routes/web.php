b9026f18862ea008fc9d7131532223f00be9bda0
Alejandro Erazo <adrian.alejo1994@hotmail.com>
2019-10-31 16:22:52 -0500

    COMENTARIOS

--- BenchMark/routes/web.php
+++ BenchMark/routes/web.php
@@ -18,9 +18,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-//Route::get('/home', 'HomeController@index')->name('home');
-//Route::get('/categorias', 'CategoriasController@index');
-//Route::get('/categorias/create', 'CategoriasController@create');
 
 Route::get('/admin', function () {
     return view('admin');
