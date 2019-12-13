fa738795b0dd63d66ef464bdffe88143354b41c9
PuceRicardo <55008586+PuceRicardo@users.noreply.github.com>
2019-12-12 23:57:59 -0500

    Update Conexion.sql

--- Conexion.sql
+++ Conexion.sql
@@ -8,3 +8,13 @@
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
 ),
+'mysql' => array(
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'database', <------ Default name for database
+    'username'  => 'root',
+    'password'  => '',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+),
