b4957b29a258ec7b663ba08c747a7ca99e1375f9
PuceRicardo <55008586+PuceRicardo@users.noreply.github.com>
2019-12-13 00:03:25 -0500

    Update Conexion.sql

--- Conexion.sql
+++ Conexion.sql
@@ -18,3 +18,25 @@
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
 ),
+
+APP_ENV=local
+APP_DEBUG=true
+APP_KEY=YOUR_API_KEY
+
+DB_HOST=YOUR_HOST
+DB_DATABASE=PROYECTO
+DB_USERNAME=ricky
+DB_PASSWORD=
+
+CACHE_DRIVER=file
+SESSION_DRIVER=file
+QUEUE_DRIVER=sync
+
+DB::connection('system')->table('tenants')->all();
+
+config(['database.connections.tenant.database' => 'tenant1']);
+
+DB::purge('tenant');
+
+DB::reconnect('tenant');
+
