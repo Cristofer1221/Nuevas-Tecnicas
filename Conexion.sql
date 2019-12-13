e17866135575f695c8194776b7b37665a04fd27d
PuceRicardo <55008586+PuceRicardo@users.noreply.github.com>
2019-12-13 00:16:26 -0500

    Update Conexion.sql

--- Conexion.sql
+++ Conexion.sql
@@ -29,11 +29,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+
             $table->timestamps();
         });
     }
@@ -65,8 +61,6 @@ class CreatePasswordResetsTable extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
     }
